df429b7494eb274ed96c1d4a3814ebe62f5eb552
Shannon McIntosh <smcintosh881@gmail.com>
2017-07-13 20:15:54 -0700

    update menu to nav bar

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,32 +1,34 @@
 import React, { Component } from 'react'
 import { Menu, Image, Button } from 'semantic-ui-react'
 
-export default class MenuExampleSecondaryPointing extends Component {
-  state = { activeItem: 'home' }
+export default class NavBar extends Component {
+  state = {}
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name })
+    document.getElementById(name).scrollIntoView() 
+  }
 
   handleRegisterClick = (e, { name }) => window.open('https://docs.google.com/forms/d/1m9iiJHQfPDUsvzSHVKbVS4ROC8WTn_kIe1qYK5D0mAo/viewform?edit_requested=true')
-
 
   render() {
     const { activeItem } = this.state
 
     return (
-        <Menu fixed='top' inverted color='violet' size='massive'>
+      <Menu fixed='top' inverted color='violet' size='massive'>
+        <Menu.Item name='landing' onClick={this.handleItemClick}> 
+          <Image src={require('../../public/backup.jpg')} verticalAlign='middle' />
+        </Menu.Item>
+        <Menu.Menu position='right'>
+          <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} />
+          <Menu.Item name='schedule' active={activeItem === 'schedule'} onClick={this.handleItemClick} />
+          <Menu.Item name='faq' active={activeItem === 'faq'} onClick={this.handleItemClick} />
+          <Menu.Item name='sponsors' active={activeItem === 'sponsors'} onClick={this.handleItemClick} />
           <Menu.Item>
-            <Image src={require('../../public/backup.jpg')} verticalAlign='middle' />
+            <Button color='yellow' basic onClick={this.handleRegisterClick}>Register</Button>
           </Menu.Item>
-          <Menu.Menu position='right'>
-            <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} />
-            <Menu.Item name='schedule' active={activeItem === 'schedule'} onClick={this.handleItemClick} />
-            <Menu.Item name='faq' active={activeItem === 'faq'} onClick={this.handleItemClick} />
-            <Menu.Item name='sponsors' active={activeItem === 'sponsors'} onClick={this.handleItemClick} />
-            <Menu.Item>
-              <Button color='yellow' basic onClick={this.handleRegisterClick}>Register</Button>
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu>
+        </Menu.Menu>
+      </Menu>
     )
   }
 }
